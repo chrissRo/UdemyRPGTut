6424b9a293b8559df3aca3bd00c8bffa702a9365
chrissRo <christian.pritzl@outlook.de>
2018-07-14 00:15:53 +0200

    lection6 simpleCamera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public GameObject Player;
+	private Vector3 offset;
+	public float rotationValue = 0.8f; 
+
+	// Use this for initialization
+	void Start () {
+
+		if (Player == null)
+		{
+			Debug.LogWarning("Please add the CameraArm");
+		}
+		else
+		{
+			offset = this.transform.position - Player.transform.position; 
+		}
+		
+	}
+
+
+	private void LateUpdate()
+	{
+		this.transform.position = Player.transform.position + offset;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Player.transform.position), Time.time * rotationValue);
+	}
+}
