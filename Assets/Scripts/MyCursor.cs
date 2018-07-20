274082a08335dd39db3cc22a440a1254e3274909
chrissRo <christian.pritzl@outlook.de>
2018-07-21 00:04:50 +0200

    end of Video 10 -> raycasting

---
+++ Assets/Scripts/MyCursor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCursor : MonoBehaviour
+{
+
+	public CameraRaycaster cr; 
+
+	// Use this for initialization
+	void Start () {
+
+		if (cr == null)
+		{
+			Debug.Log("Please asign the CameraRaycaster-Object to Cursor.cs");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			Debug.Log("LayerHit is: " + cr.layerHit);
+	}
+}
